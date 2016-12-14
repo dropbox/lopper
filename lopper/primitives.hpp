17923816c60bf62974450c2babc6be8987c036f7
Jongmin Baek <jongmin@dropbox.com>
2016-12-14 15:32:42 -0800

    Implement vector primitives on AVX

--- lopper/primitives.hpp
+++ lopper/primitives.hpp
@@ -40,6 +40,9 @@ namespace lopper {
   template<InstructionSet S> SINT32 VLOAD(const uint8_t* addr);
   template<InstructionSet S> SFLOAT VSET(float op);
   template<InstructionSet S> SINT32 VSET(int32_t op);
+  template<InstructionSet S> SINT32 VSET8(uint8_t op) {
+    return VSET<S>(op * 0x01010101);
+  }
   template<InstructionSet S> SINT32 VSET8x16(uint8_t op_a, uint8_t op_b, uint8_t op_c, uint8_t op_d,
                                              uint8_t op_e, uint8_t op_f, uint8_t op_g, uint8_t op_h,
                                              uint8_t op_i, uint8_t op_j, uint8_t op_k, uint8_t op_l,
@@ -64,7 +67,11 @@ namespace lopper {
   template<InstructionSet S> SFLOAT VLOOKUP_FP_ARRAY(const float* arr, SINT32 index); // Return arr[index] without boundary checking.
   template<InstructionSet S> SINT32 VSELECT(SINT32 mask, SINT32 op1, SINT32 op2); // mask==0?op1:op2
   template<InstructionSet S> SFLOAT VSELECT(SINT32 mask, SFLOAT op1, SFLOAT op2); // mask==0?op1:op2
-  template<typename T> uint32_t VNARROW_TO_8BIT(T op); // Takes the least significant 8-bit of each lane and compact.
+  // Takes the least significant 8-bit of each lane and compact.
+  template<InstructionSet S> typename std::enable_if<InstructionSetTrait<S>::num_lanes == 1, uint8_t>::type VNARROW_TO_8BIT(SINT32 op);
+  template<InstructionSet S> typename std::enable_if<InstructionSetTrait<S>::num_lanes == 4, uint32_t>::type VNARROW_TO_8BIT(SINT32 op);
+  template<InstructionSet S> typename std::enable_if<InstructionSetTrait<S>::num_lanes == 8, uint64_t>::type VNARROW_TO_8BIT(SINT32 op);
+
   template<InstructionSet S> SINT32 VSHUFFLE(SINT32 op1, SINT32 index);
   template<InstructionSet S> SFLOAT VCAST_FLOAT(SINT32 op1);
 }
@@ -134,7 +141,7 @@ namespace lopper {
   template<> inline float VLOOKUP_FP_ARRAY<SCALAR>(const float* arr, int32_t index) { return arr[index]; }
   template<> inline float VSELECT<SCALAR>(int32_t mask, float op1, float op2) { return mask==0?op1:op2; }
   template<> inline int32_t VSELECT<SCALAR>(int32_t mask, int32_t op1, int32_t op2) { return mask==0?op1:op2; }
-  template<> inline uint32_t VNARROW_TO_8BIT(int32_t op) { return op & 0x0ff; }
+  template<> inline uint8_t VNARROW_TO_8BIT<SCALAR>(int32_t op) { return op & 0x0ff; }
 
   template<size_t C> inline int32_t VGET_LANE(int32_t op) { return op; }
   template<size_t C> inline float VGET_LANE(float op) { return op; }
@@ -162,7 +169,8 @@ namespace lopper {
 // NOTE(jongmin): Clang has an open bug (#20670) yielding overzealous alignment warning when making SSE calls
 // for loading and storing, so we cast to void* first as a workaround.
 
-#if defined LOPPER_TARGET_SSE4
+// As AVX needs to fall back to SSE4 in case of integer ops, we define ops for AVX as well.
+#if defined(LOPPER_TARGET_SSE4) || defined(LOPPER_TARGET_AVX)
 namespace lopper {
   template<> inline __m128 VADD(__m128 op1, __m128 op2) { return _mm_add_ps(op1, op2); }
   template<> inline __m128 VSUB(__m128 op1, __m128 op2) { return _mm_sub_ps(op1, op2); }
@@ -253,15 +261,198 @@ namespace lopper {
     return _mm_or_si128(_mm_and_si128(is_zero, op1),
                         _mm_andnot_si128(is_zero, op2));
   }
-  template<size_t C> inline int32_t VGET_LANE(__m128i op) { return _mm_extract_epi32(op, C); }
-  template<size_t C> inline float VGET_LANE(__m128 op) { union { int32_t i; float f; } tmp; tmp.i = _mm_extract_ps(op, C); return tmp.f; }
-  template<> inline uint32_t VNARROW_TO_8BIT(__m128i op) {
+  template<size_t C> inline int32_t VGET_LANE(__m128i op) { return _mm_extract_epi32(op, C % 4); }
+  template<size_t C> inline float VGET_LANE(__m128 op) { union { int32_t i; float f; } tmp; tmp.i = _mm_extract_ps(op, C % 4); return tmp.f; }
+  template<> inline uint32_t VNARROW_TO_8BIT<SSE>(__m128i op) {
     return (uint32_t)VGET_LANE<0>(_mm_shuffle_epi8(op, _mm_setr_epi8(0, 4, 8, 12, 255u, 255u, 255u, 255u,
                                                                      255u, 255u, 255u, 255u, 255u, 255u, 255u, 255u)));
   }
   template<> inline __m128 VADD_PAIRWISE(__m128 op1, __m128 op2) { return _mm_hadd_ps(op1, op2); }
   template<> inline __m128i VSHUFFLE<SSE>(__m128i op1, __m128i index) { return _mm_shuffle_epi8(op1, index); }
   template<> inline __m128 VCAST_FLOAT<SSE>(__m128i op1) { return _mm_castsi128_ps(op1); }
+}
+#endif
+
+/*======================================================*/
+//            Inline implementation for AVX
+/*======================================================*/
+#if defined LOPPER_TARGET_AVX
+namespace lopper {
+  // Some helpers to make delegating to SSE easier.
+  inline __m256i _VCONCAT(__m128i op1, __m128i op2) { return _mm256_setr_m128i(op1, op2); }
+  inline __m128i _VLO(__m256i op1) { return _mm256_castsi256_si128(op1); }
+  inline __m128i _VHI(__m256i op1) { return _mm256_extractf128_si256(op1, 1); }
+
+  template<> inline __m256 VADD(__m256 op1, __m256 op2) { return _mm256_add_ps(op1, op2); }
+  template<> inline __m256 VSUB(__m256 op1, __m256 op2) { return _mm256_sub_ps(op1, op2); }
+  template<> inline __m256 VMUL(__m256 op1, __m256 op2) { return _mm256_mul_ps(op1, op2); }
+  template<> inline __m256 VDIV(__m256 op1, __m256 op2) { return _mm256_div_ps(op1, op2); }
+  template<> inline __m256 VDIV_FAST(__m256 op1, __m256 op2) { return _mm256_mul_ps(op1, _mm256_rcp_ps(op2)); }
+  template<> inline __m256 VMIN(__m256 op1, __m256 op2) { return _mm256_min_ps(op1, op2); }
+  template<> inline __m256 VMAX(__m256 op1, __m256 op2) { return _mm256_max_ps(op1, op2); }
+  template<> inline __m256 VABS(__m256 op1) { return _mm256_andnot_ps(_mm256_set1_ps(-0.f), op1); }
+  template<> inline void VSTORE(float* addr, __m256 op) { _mm256_storeu_ps(addr, op); }
+  template<> inline void VSTORE_ALIGNED(float* addr, __m256 op) { _mm256_store_ps(addr, op); }
+  template<> inline __m256 VLOAD<AVX>(const float* addr) { return _mm256_loadu_ps(addr); }
+  template<> inline __m256 VSET<AVX>(float op) { return _mm256_set1_ps(op); }
+
+#define LOPPER_SSE4_LANEWISE_UNARY_WRAPPER_FOR_AVX(OP) \
+  template<> inline __m256i OP(__m256i op1) { \
+    return _mm256_setr_m128i(OP(_mm256_castsi256_si128(op1)), \
+                             OP(_mm256_extractf128_si256(op1, 1))); }
+  // XXX: Probably should test if _mm256_castsi256_si128 is actually more performant.
+#define LOPPER_SSE4_LANEWISE_BINARY_WRAPPER_FOR_AVX(OP) \
+  template<> inline __m256i OP(__m256i op1, __m256i op2) { \
+    return _mm256_setr_m128i(OP(_mm256_castsi256_si128(op1), _mm256_castsi256_si128(op2)), \
+                             OP(_mm256_extractf128_si256(op1, 1), _mm256_extractf128_si256(op2, 1))); }
+  LOPPER_SSE4_LANEWISE_BINARY_WRAPPER_FOR_AVX(VADD);
+  LOPPER_SSE4_LANEWISE_BINARY_WRAPPER_FOR_AVX(VSUB);
+  LOPPER_SSE4_LANEWISE_BINARY_WRAPPER_FOR_AVX(VMUL);
+  LOPPER_SSE4_LANEWISE_BINARY_WRAPPER_FOR_AVX(VDIV);
+  LOPPER_SSE4_LANEWISE_BINARY_WRAPPER_FOR_AVX(VDIV_FAST);
+  LOPPER_SSE4_LANEWISE_BINARY_WRAPPER_FOR_AVX(VMIN);
+  LOPPER_SSE4_LANEWISE_BINARY_WRAPPER_FOR_AVX(VMAX);
+  LOPPER_SSE4_LANEWISE_UNARY_WRAPPER_FOR_AVX(VABS);
+#undef LOPPER_SSE4_LANEWISE_UNARY_WRAPPER_FOR_AVX
+#undef LOPPER_SSE4_LANEWISE_BINARY_WRAPPER_FOR_AVX
+
+  template<> inline void VSTORE(int32_t* addr, __m256i op) {
+    VSTORE(addr, _VLO(op));
+    VSTORE(addr + 4, _VHI(op));
+  }
+  template<> inline void VSTORE_ALIGNED(int32_t* addr, __m256i op) {
+    VSTORE_ALIGNED(addr, _VLO(op));
+    VSTORE_ALIGNED(addr + 4, _VHI(op));
+  }
+  template<> inline void VSTORE(uint8_t* addr, __m256i op) {
+    VSTORE(addr, _VLO(op));
+    VSTORE(addr + 16, _VHI(op));
+  }
+  template<> inline void VSTORE_ALIGNED(uint8_t* addr, __m256i op) {
+    VSTORE_ALIGNED(addr, _VLO(op));
+    VSTORE_ALIGNED(addr + 16, _VHI(op));
+  }
+  template<> inline __m256i VLOAD<AVX>(const int32_t* addr) {
+    return _VCONCAT(VLOAD<SSE>(addr), VLOAD<SSE>(addr + 4));
+  }
+  template<> inline __m256i VLOAD<AVX>(const uint8_t* addr) {
+    return _VCONCAT(VLOAD<SSE>(addr), VLOAD<SSE>(addr + 16));
+  }
+  template<> inline __m256i VSET<AVX>(int32_t op) {
+    return _VCONCAT(VSET<SSE>(op), VSET<SSE>(op));
+  }
+  template<> inline __m256i VSET8x16<AVX>(uint8_t op_a, uint8_t op_b, uint8_t op_c, uint8_t op_d,
+                                          uint8_t op_e, uint8_t op_f, uint8_t op_g, uint8_t op_h,
+                                          uint8_t op_i, uint8_t op_j, uint8_t op_k, uint8_t op_l,
+                                          uint8_t op_m, uint8_t op_n, uint8_t op_o, uint8_t op_p) {
+    return _VCONCAT(_mm_setr_epi8(op_a, op_b, op_c, op_d, op_e, op_f, op_g, op_h,
+                                  op_i, op_j, op_k, op_l, op_m, op_n, op_o, op_p),
+                    VSET<SSE>(0));
+  }
+  template<> inline __m256 VTO_FLOAT<AVX>(__m256i op1) { return _mm256_cvtepi32_ps(op1); }
+  template<> inline __m256i VTO_INT32<AVX>(__m256 op1) { return _mm256_cvttps_epi32(op1); }
+  template<> inline __m256 VCAST_FLOAT<AVX>(__m256i op1) { return _mm256_castsi256_ps(op1); }
+
+  template<> inline __m256i VEXPAND_QTR<AVX, 0>(__m256i op) {
+    return _VCONCAT(VEXPAND_QTR<SSE, 0>(_VLO(op)), VEXPAND_QTR<SSE, 1>(_VLO(op)));
+  }
+  template<> inline __m256i VEXPAND_QTR<AVX, 1>(__m256i op) {
+    return _VCONCAT(VEXPAND_QTR<SSE, 2>(_VLO(op)), VEXPAND_QTR<SSE, 3>(_VLO(op)));
+  }
+  template<> inline __m256i VEXPAND_QTR<AVX, 2>(__m256i op) {
+    return _VCONCAT(VEXPAND_QTR<SSE, 0>(_VHI(op)), VEXPAND_QTR<SSE, 1>(_VHI(op)));
+  }
+  template<> inline __m256i VEXPAND_QTR<AVX, 3>(__m256i op) {
+    return _VCONCAT(VEXPAND_QTR<SSE, 2>(_VHI(op)), VEXPAND_QTR<SSE, 3>(_VHI(op)));
+  }
+  template<> inline __m256i VCOLLAPSE_TO_BYTES<AVX>(__m256i op1, __m256i op2, __m256i op3, __m256i op4) {
+    return _VCONCAT(VCOLLAPSE_TO_BYTES<SSE>(_VLO(op1), _VHI(op1), _VLO(op2), _VHI(op2)),
+                    VCOLLAPSE_TO_BYTES<SSE>(_VLO(op3), _VHI(op3), _VLO(op4), _VHI(op4)));
+  }
+  template<> inline __m256i VBITWISE_OR(__m256i op1, __m256i op2) {
+    return _mm256_castps_si256(_mm256_or_ps(VCAST_FLOAT<AVX>(op1), VCAST_FLOAT<AVX>(op2)));
+  }
+  template<> inline __m256i VBITWISE_AND(__m256i op1, __m256i op2) {
+    return _mm256_castps_si256(_mm256_and_ps(VCAST_FLOAT<AVX>(op1), VCAST_FLOAT<AVX>(op2)));
+  }
+  template<size_t bits> inline __m256i VSHIFTL(__m256i op1) {
+    return _VCONCAT(VSHIFTL<bits>(_VLO(op1)), VSHIFTL<bits>(_VHI(op1)));
+  }
+  template<size_t bits> inline __m256i VSHIFTR(__m256i op1) {
+    return _VCONCAT(VSHIFTR<bits>(_VLO(op1)), VSHIFTR<bits>(_VHI(op1)));
+  }
+  template<> inline __m256i VLT<AVX>(__m256i op1, __m256i op2) {
+    return _VCONCAT(VLT<SSE>(_VLO(op1), _VLO(op2)), VLT<SSE>(_VHI(op1), _VHI(op2)));
+  }
+  template<> inline __m256i VLT<AVX>(__m256 op1, __m256 op2) {
+    return _mm256_castps_si256(_mm256_cmp_ps(op1, op2, 17 /* _CMP_LT_OQ, meaning NaN is unsignaled */));
+  }
+  template<> inline __m256i VEQ<AVX>(__m256i op1, __m256i op2) {
+    return _VCONCAT(VEQ<SSE>(_VLO(op1), _VLO(op2)), VEQ<SSE>(_VHI(op1), _VHI(op2)));
+  }
+  template<> inline __m256i VEQ<AVX>(__m256 op1, __m256 op2) {
+    return _mm256_castps_si256(_mm256_cmp_ps(op1, op2, 0 /* _CMP_EQ_OQ, meaning NaN is unsignaled */));
+  }
+  template<> inline __m256 VLDEXP<AVX>(__m256 s, __m256i e) {
+    // Need to increase the exponent for the floating-point s by e. This may overflow.
+    __m256 MASK_EXPONENT = VCAST_FLOAT<AVX>(VSET<AVX>(0x7f800000));
+    __m256i exponent = VSHIFTR<23>(_mm256_castps_si256(_mm256_and_ps(s, MASK_EXPONENT)));
+    return _mm256_or_ps(_mm256_andnot_ps(MASK_EXPONENT, s),
+                        VCAST_FLOAT<AVX>(VSHIFTL<23>(VADD(exponent, e))));
+  }
+  template<> inline __m256 VFREXP<AVX>(__m256 a, __m256i& iexp) {
+    __m256 MASK_EXPONENT = VCAST_FLOAT<AVX>(VSET<AVX>(0x7f800000));
+    __m256 significand = _mm256_or_ps(_mm256_andnot_ps(MASK_EXPONENT, a),
+                                      VCAST_FLOAT<AVX>(VSHIFTL<23>(VSET<AVX>(127))));
+    iexp = VSUB(VSHIFTR<23>(_mm256_castps_si256(_mm256_and_ps(a, MASK_EXPONENT))), VSET<AVX>(127));
+    return significand;
+  }
+  template<> inline __m256 VLOOKUP_FP_ARRAY<AVX>(const float* arr, __m256i index) {
+    int32_t indices[8] __attribute__((aligned(32)));
+    VSTORE_ALIGNED(indices, index);
+    return _mm256_set_ps(arr[indices[7]], arr[indices[6]], arr[indices[5]], arr[indices[4]],
+                         arr[indices[3]], arr[indices[2]], arr[indices[1]], arr[indices[0]]);
+  }
+  template<> inline __m256 VSELECT<AVX>(__m256i mask, __m256 op1, __m256 op2) {
+    const __m256 is_zero = _mm256_cmp_ps(VCAST_FLOAT<AVX>(mask), _mm256_setzero_ps(), 0 /* _CMP_EQ_OQ */);
+    return _mm256_or_ps(_mm256_and_ps(is_zero, op1),
+                        _mm256_andnot_ps(is_zero, op2));
+  }
+  template<> inline __m256i VSELECT<AVX>(__m256i mask, __m256i op1, __m256i op2) {
+    return _mm256_castps_si256(VSELECT<AVX>(mask, VCAST_FLOAT<AVX>(op1), VCAST_FLOAT<AVX>(op2)));
+  }
+
+  template<size_t C> inline int32_t VGET_LANE(__m256i op) { return _mm256_extract_epi32(op, C % 8); }
+  template<size_t C> inline float VGET_LANE(__m256 op) {
+    union { int32_t i; float f; } tmp;
+    tmp.i = VGET_LANE<C>(_mm256_castps_si256(op));
+    return tmp.f;
+  }
+  template<> inline uint64_t VNARROW_TO_8BIT<AVX>(__m256i op) {
+    const uint64_t lo = static_cast<uint64_t>(VNARROW_TO_8BIT<SSE>(_VLO(op)));
+    const uint64_t hi = static_cast<uint64_t>(VNARROW_TO_8BIT<SSE>(_VHI(op)));
+    return lo | (hi << 32);
+  }
+  template<> inline __m256 VADD_PAIRWISE(__m256 op1, __m256 op2) { return _mm256_hadd_ps(op1, op2); }
+  template<> inline __m256i VSHUFFLE<AVX>(__m256i op1, __m256i index) {
+    // Note that even AVX2's _mm256_shuffle_epi8 actually only shuffles within each half
+    // so we need to resort to some trickery.
+    // A bitmask of 0x9F=159 corresponds to bits actually considered in SSE's shuffle operation.
+    const __m256i filtered_index = VBITWISE_AND(VSET8<AVX>(159u), index);
+    const __m256i flag = _VCONCAT(_mm_cmplt_epi8(VSET8<SSE>(15), _VLO(filtered_index)),
+                                  _mm_cmplt_epi8(VSET8<SSE>(15), _VHI(filtered_index)));
+    // flag will be set if filtered index >= 16, but not if it's 128+ due to signed comparison.
+    // This is OK, though.
+    const __m256i inverted_flag = _mm256_castps_si256(_mm256_xor_ps(VCAST_FLOAT<AVX>(flag),
+                                                                    VCAST_FLOAT<AVX>(VSET8<AVX>(255u))));
+    const auto index_under_16 = VBITWISE_OR(filtered_index, flag);
+    const auto index_over_16 = VBITWISE_AND(VSET8<AVX>(239u), VBITWISE_OR(filtered_index, inverted_flag));
+    auto result_from_first = _VCONCAT(VSHUFFLE<SSE>(_VLO(op1), _VLO(index_under_16)),
+                                      VSHUFFLE<SSE>(_VLO(op1), _VHI(index_under_16)));
+    auto result_from_second = _VCONCAT(VSHUFFLE<SSE>(_VHI(op1), _VLO(index_over_16)),
+                                       VSHUFFLE<SSE>(_VHI(op1), _VHI(index_over_16)));
+    return VBITWISE_OR(result_from_first, result_from_second);
+  }
 }
 #endif
 
@@ -381,9 +572,9 @@ namespace lopper {
                                            vbicq_u32(vreinterpretq_u32_s32(op2), is_zero)));
   }
 
-  template<size_t C> inline int32_t VGET_LANE(int32x4_t op) { return vgetq_lane_s32(op, C); }
-  template<size_t C> inline float VGET_LANE(float32x4_t op) { return vgetq_lane_f32(op, C); }
-  template<> inline uint32_t VNARROW_TO_8BIT(int32x4_t op) {
+  template<size_t C> inline int32_t VGET_LANE(int32x4_t op) { return vgetq_lane_s32(op, C % 4); }
+  template<size_t C> inline float VGET_LANE(float32x4_t op) { return vgetq_lane_f32(op, C % 4); }
+  template<> inline uint32_t VNARROW_TO_8BIT<NEON>(int32x4_t op) {
     return vget_lane_u32(vreinterpret_u32_u8(vmovn_u16(vcombine_u16(vmovn_u32(vreinterpretq_u32_s32(op)), vcreate_u16(0)))), 0);
   }
   template<> inline float32x4_t VADD_PAIRWISE(float32x4_t op1, float32x4_t op2) {
